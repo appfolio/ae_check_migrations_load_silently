appfolio/ae_fast_decimal_formatter

build: migrate gem source from RubyGems to JFrog

Appraisals
@@ -2,17 +2,17 @@
 
 if Gem::Requirement.new(['>= 3.3', '< 4.1']).satisfied_by?(Gem::Version.new(RUBY_VERSION))
   appraise "ruby-#{RUBY_VERSION}__actionview_72" do
-    source 'https://rubygems.org' do
+    source 'https://appfolio.jfrog.io/artifactory/api/gems/appfolio-ae_fast_decimal_formatter-gem/' do
       gem 'actionview', '~> 7.2.0'
     end
   end
   appraise "ruby-#{RUBY_VERSION}__actionview_80" do
-    source 'https://rubygems.org' do
+    source 'https://appfolio.jfrog.io/artifactory/api/gems/appfolio-ae_fast_decimal_formatter-gem/' do
       gem 'actionview', '~> 8.0.0'
     end
   end
   appraise "ruby-#{RUBY_VERSION}__actionview_81" do
-    source 'https://rubygems.org' do
+    source 'https://appfolio.jfrog.io/artifactory/api/gems/appfolio-ae_fast_decimal_formatter-gem/' do
       gem 'actionview', '~> 8.1.0'
     end
   end

Gemfile
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-source 'https://rubygems.org' # global source
+source 'https://appfolio.jfrog.io/artifactory/api/gems/appfolio-ae_fast_decimal_formatter-gem/'
 
-source 'https://rubygems.org' do
+source 'https://appfolio.jfrog.io/artifactory/api/gems/appfolio-ae_fast_decimal_formatter-gem/' do
   gem 'actionview', '>= 7.2', '< 8.2'
   gem 'activesupport', '>= 7.2', '< 8.2'
   gem 'appraisal', '>= 2.5', '< 3'

appfolio/ae_check_migrations_load_silently

Appraisals
@@ -2,19 +2,19 @@
 
 if Gem::Requirement.new(['>= 3.3', '< 4.1']).satisfied_by?(Gem::Version.new(RUBY_VERSION))
   appraise "ruby-#{RUBY_VERSION}_activerecord72" do
-    source 'https://rubygems.org' do
+    source 'https://appfolio.jfrog.io/artifactory/api/gems/appfolio-ae_check_migrations_load_silently-gem/' do
       gem 'activerecord', '~> 7.2.0'
     end
   end
 
   appraise "ruby-#{RUBY_VERSION}_activerecord80" do
-    source 'https://rubygems.org' do
+    source 'https://appfolio.jfrog.io/artifactory/api/gems/appfolio-ae_check_migrations_load_silently-gem/' do
       gem 'activerecord', '~> 8.0.0'
     end
   end
 
   appraise "ruby-#{RUBY_VERSION}_activerecord81" do
-    source 'https://rubygems.org' do
+    source 'https://appfolio.jfrog.io/artifactory/api/gems/appfolio-ae_check_migrations_load_silently-gem/' do
       gem 'activerecord', '~> 8.1.0'
     end
   end

Gemfile
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-source 'https://rubygems.org' # global source
+source 'https://appfolio.jfrog.io/artifactory/api/gems/appfolio-ae_check_migrations_load_silently-gem/'
 
-source 'https://rubygems.org' do
+source 'https://appfolio.jfrog.io/artifactory/api/gems/appfolio-ae_check_migrations_load_silently-gem/' do
   gem 'appraisal', '>= 2.5', '< 3'
   gem 'bundler', '>= 2.6', '< 5'
   gem 'debug', '>= 1.11', '< 2'